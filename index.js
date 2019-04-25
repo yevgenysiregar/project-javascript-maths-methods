f3fddadb1b6ab7f934ee5645ed2d495552ba8b86
Yevgeny Omar Siregar <siregar.yevgeny@gmail.com>
2019-04-25 21:03:44 +0700

    Add code on index.js to try using maths methods

---
+++ index.js
@@ -0,0 +1,22 @@
+const num1 = 3.6;
+const num2 = 3.2;
+const num3 = 4.5;
+const num4 = 4.2;
+
+console.log(Math.floor(num1));
+console.log(Math.ceil(num2));
+console.log(Math.round(num3));
+console.log(Math.trunc(num4));
+
+const trackLength = "100m";
+console.log(parseInt(trackLength));
+
+const bodyHeight = "172.5cm";
+console.log(parseFloat(bodyHeight));
+
+alert(Math.random());
+
+console.log(Math.max(5, 10, -5, 0));
+console.log(Math.min(5, 10, -5, 0));
+
+console.log(Math.pow(2, 5));
